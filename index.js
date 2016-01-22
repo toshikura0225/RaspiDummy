c38e08f3e39d8f44061ce3fa8f4edd21eb028384
toshikura0225 <t_kuramata@yahoo.co.jp>
2016-01-23 07:33:59 +0900

    index.jsを追加

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var http_src = fs.readFileSync('./index.html');
+
+var app = http.createServer(function(req, res) {
+	
+	var url_parts = url.parse(req.url);
+	
+	console.log(url_parts.pathname);
+	
+	if(url_parts.pathname == '/')
+	{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(http_src);
+		res.end();
+	}
+	else
+	{
+		res.end();
+	}
+
+	
+}).listen(process.env.PORT || 3000);

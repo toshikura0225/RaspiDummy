306c0c2e673b4b354c042cd0079ef23663d07dc6
toshikura0225 <t_kuramata@yahoo.co.jp>
2016-02-09 23:37:00 +0900

    socket.io-clientテストOK

--- index.js
+++ index.js
@@ -29,6 +29,24 @@ io.sockets.on('connection', function(socket) {
   socket.on('msg', function(data) {
     //io.sockets.emit('msg', data);
 	socket.broadcast.emit('msg', data);
+	console.log("to server msg" + data);
+  });
+    socket.on('abc', function(data) {
+    //io.sockets.emit('msg', data);
+	//socket.broadcast.emit('msg', data);
+	console.log("to server abc" + data);
+  });
+});
+
+var client = require('socket.io-client');
+var cli_socket = client.connect('http://localhost:3000');
+cli_socket.on('connect',function(socket){
+    cli_socket.send('how are you?');
+ //   cli_socket.disconnect();
+ //   process.exit(0);
+   cli_socket.on('msg', function(data) {
+cli_socket.emit('abc', "thank you");
+	console.log('to client' + data);
   });
 });
 

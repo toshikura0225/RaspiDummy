43f9ea9319f63c00cda8fdeeb9a07c71c088a1eb
toshikura0225 <t_kuramata@yahoo.co.jp>
2016-02-21 14:52:58 +0900

    child_processで実装

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+// こんなものもある
+var spawn = require('child_process').spawn,
+    py    = spawn('python', ['test.py']),
+    data = [1,2,3,4,5,6,7,8,9],
+    dataString = '';
 
 var http_src = fs.readFileSync('./index.html');
 
@@ -27,28 +32,30 @@ var app = http.createServer(function(req, res) {
 
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
-  socket.on('message', function(data) {		// messageイベント：すべてのメッセージを受信時
+	socket.on('message', function(data) {		// messageイベント：すべてのメッセージを受信時
     //io.sockets.emit('msg', data);
-	socket.broadcast.emit('msg', data);
-	console.log("to server msg" + data);
-  });
-    socket.on('abc', function(data) {
+		socket.broadcast.emit('msg', data);
+		console.log("to server msg" + data);
+	});
+	socket.on('abc', function(data) {
     //io.sockets.emit('msg', data);
 	//socket.broadcast.emit('msg', data);
-	console.log("to server abc" + data);
+		console.log("to server abc" + data);
+		py.stdin.write("OKK");
+//	py.stdin.end();
   });
 });
 
 var client = require('socket.io-client');
 var cli_socket = client.connect('http://localhost:3000');
 cli_socket.on('connect',function(socket){
-    cli_socket.send('how are you?');
+	cli_socket.send('how are you?');
  //   cli_socket.disconnect();
  //   process.exit(0);
-   cli_socket.on('msg', function(data) {
-cli_socket.emit('abc', "thank you");
-	console.log('to client' + data);
-  });
+	cli_socket.on('msg', function(data) {
+		cli_socket.emit('abc', "thank you");
+		console.log('to client' + data);
+	});
 });
 /*
 var tk_spawn = require("child_process").spawn;
@@ -58,6 +65,7 @@ tk_process.stdout.on('data', function (data){
 });
 */
 
+/*
 var PythonShell = require('python-shell');
  
 var options = {
@@ -81,21 +89,13 @@ pyshell.stdout.on('data', function(data) {
     pyshell.send('go');
     console.log("on." + data);
 });
+*/
 
-
-// こんなものもある
-var spawn = require('child_process').spawn,
-    py    = spawn('python', ['test.py']),
-    data = [1,2,3,4,5,6,7,8,9],
-    dataString = '';
 
 py.stdout.on('data', function(data){
   console.log("on." + data);
 });
-py.stdout.on('end', function(){
-  console.log('Sum of numbers=',dataString);
-});
-py.stdin.write(JSON.stringify(data));
-py.stdin.end();
+//py.stdin.write(JSON.stringify(data));
+//py.stdin.end();
 
 console.log('Server running!');
